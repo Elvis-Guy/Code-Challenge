4507611f47cb19b53c65ef76f7fb825f666e0944
Elvis Guy Bakunzi <guyelvisbakunzi@gmail.com>
2022-11-30 17:36:26 +0200

    some changes

---
+++ programCheapest.js
@@ -0,0 +1,25 @@
+const items =[
+    { name: "Bike", price: 100},
+    { name: "Tv", price: 200},
+    { name: "Album", price: 10},
+    { name: "Book", price: 5},
+    { name: "Phone", price: 500},
+    { name: "Computer", price: 1000},
+   
+   ];
+    
+   var a = 1;
+   var b = 0;
+   one = items[0].price;
+   
+   while (a < 6){
+       if (items[a].price < one ){
+                b = a;
+                   a++;
+       }
+       else {
+           
+           a++;
+       }    
+      }
+      console.log('The Cheapest item  is ${items[b].name} and it costs ${items[b].price');
