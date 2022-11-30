a3de3721ea9516726545c095bfddf8569ae9e35c
Elvis Guy Bakunzi <guyelvisbakunzi@gmail.com>
2022-11-30 16:56:43 +0200

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,18 @@
+const items =[
+ { name: "Bike", price: 100},
+ { name: "Tv", price: 200},
+ { name: "Album", price: 10},
+ { name: "Book", price: 5},
+ { name: "Phone", price: 500},
+ { name: "Computer", price: 1000},
+
+];
+ 
+var a = 0;
+var sum = 0;
+if (a=0, a < 6, a++)
+{   
+sum = sum + items[a].price;
+
+}
+console.log ('the price is ${sum}');
